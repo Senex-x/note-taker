07f84529ac4f264314c78606630421cf692c63d4
V-Senex <junior.07.inator@gmail.com>
2023-01-21 19:44:03 +0300

    Recycler view cleanup

--- app/src/main/java/com/senex/notetaker/notes/NotesFragment.kt
+++ app/src/main/java/com/senex/notetaker/notes/NotesFragment.kt
@@ -2,15 +2,12 @@ package com.senex.notetaker.notes
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.senex.notetaker.R
 import com.senex.notetaker.databinding.FragmentNotesBinding
 import com.senex.notetaker.notes.recycler.NoteItem
-import com.senex.notetaker.notes.recycler.NoteViewHolderFactory
+import com.senex.notetaker.notes.recycler.NoteRecyclerViewHolderFactory
 import com.senex.notetaker.util.BindingFragment
-import ru.tinkoff.mobile.tech.ti_recycler.TiRecyclerBuilder
 import ru.tinkoff.mobile.tech.ti_recycler.base.ViewTyped
 import ru.tinkoff.mobile.tech.ti_recycler_coroutines.TiRecyclerCoroutines
 
@@ -21,13 +18,16 @@ class NotesFragment : BindingFragment<FragmentNotesBinding>() {
 
     override fun FragmentNotesBinding.onViewCreated() {
 
-        val recycler = TiRecyclerCoroutines<ViewTyped>(
-            recyclerView = recycler,
-            NoteViewHolderFactory(),
+        TiRecyclerCoroutines<ViewTyped>(
+            recyclerView = noteRecycler,
+            holderFactory = NoteRecyclerViewHolderFactory(),
         ) {
-            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
-        }
-        recycler.setItems(getItems())
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                RecyclerView.VERTICAL,
+                false
+            )
+        }.setItems(getItems())
 
     }
 
