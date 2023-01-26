f03e7789e6ba36470dedc751d77961ea5042de24
V-Senex <junior.07.inator@gmail.com>
2023-01-26 12:53:21 +0300

    Adjust layout for keyboard size automatically

--- app/src/main/java/com/senex/notetaker/edit/EditFragment.kt
+++ app/src/main/java/com/senex/notetaker/edit/EditFragment.kt
@@ -1,10 +1,15 @@
 package com.senex.notetaker.edit
 
 import android.content.Context
+import android.graphics.Rect
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -28,17 +33,29 @@ class EditFragment : BindingBottomSheetFragment<FragmentEditBinding>() {
     override fun FragmentEditBinding.onViewCreated() {
 
         lifecycleScope.launch {
-            delay(300)
+            delay(100)
+
             showSoftKeyboard(edit)
+
+            delay(100)
+
+            val params = root.layoutParams
+            params.height += getKeyboardHeight(root)
+            root.layoutParams = params
         }
     }
 
     // TODO: Bruh
     private fun showSoftKeyboard(view: View) {
         if (view.requestFocus()) {
-            val imm =
-                requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+            (requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
         }
+    }
+
+    // TODO: Causes visual bugs
+    private fun getKeyboardHeight(attachedView: View): Int {
+        val insets = ViewCompat.getRootWindowInsets(attachedView)
+        return insets?.getInsets(WindowInsetsCompat.Type.ime())?.bottom ?: 0
     }
 }
